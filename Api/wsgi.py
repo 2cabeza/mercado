0286ac102dbaaada518e0dc511a620e215071dea
2cabeza <72514789+2cabeza@users.noreply.github.com>
2023-11-02 10:47:22 -0300

    update text

--- Api/wsgi.py
+++ Api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'render_deploy.settings')
 
 application = get_wsgi_application()
